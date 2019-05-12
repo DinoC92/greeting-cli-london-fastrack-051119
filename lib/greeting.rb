921127d24dde3c1f3633ffb8c6f81cd84fca1c1b
Dean Burgess <dinocastiglione@ymail.com>
2019-05-12 10:18:49 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,18 @@
 # code the #greeting method here!
+def greeting(name)
+ puts "Hello #{name}. It's nice to meet you."
+end
+
+
+name= Dino
+greeting= "Hello {name}. It's nice to meet you."
+puts "Hi! I'm HAL, what's your name?"
+name = gets.strip.chomp
+gets.strip= puts
+chomp= name
+greeting= Dino 
+
+
+
+
+greeting("Ian")
